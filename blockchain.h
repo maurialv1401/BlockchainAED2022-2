094bfb77d2447febab204a4ad69bf4c06ba7cd7e
nicolas-castaneda <102196795+nicolas-castaneda@users.noreply.github.com>
2022-10-08 22:43:51 -0500

    Blockchain Implementation - Functions insert, cascade added

--- blockchain.h
+++ blockchain.h
@@ -1,44 +1,103 @@
+#include <utility>
+
 #include "block.h"
 
-// implement template and smart pointers
+// implement template and smart pointers'
+template<typename Data=int>
 class Blockchain {
 private:
-    Block * genesis;
-    Block * last;
+    // mutables
+    int size; // size of blockchain
+    Block<Data> * current;
+    // fixed
+    Block<Data> * genesis;
     int precision;
     int max_transactions;
-    int size;
     
 public:
     Blockchain(){
-        this->genesis = new Block("0", 0);
-        this->precision = 2;
+        this->genesis = new Block<Data>("000", 0);
+        this->precision = 3;
         this->max_transactions = 4;
         this->size = 1;
-        this->last = this->genesis;
+        this->current = new Block<Data>(this->max_transactions);
+        this->genesis->next = this->current;
+        this->current->set_prev_hash(this->genesis->get_hash());
+        this->current->set_precision(this->precision);
     };
-    void add_block(Block* block){
-        block->set_precision(this->precision);
-        block->set_prev_hash(this->last->get_hash());
-        block->mine();
-        // unimos los bloques
-        this->last->next = block;
-        // actualizamos el ultimo bloque
-        this->last = block;
-        this->size++;
-    }
-    void print_chain(){
-        Block * current = this->genesis->next;
-        while(current != nullptr){
-            std::cout << "Hash: " << current->get_hash() << std::endl;
-            std::cout << "Prev Hash: " << current->get_prev_hash() << std::endl;
-            std::cout << "Transaction: " << current->get_transaction() << std::endl;
-            std::cout << "Nonce: " << current->get_nonce() << std::endl;
-            std::cout << "Size: " << current->get_size() << std::endl;
-            std::cout << "----------------" << std::endl;
-            current = current->next;
+
+    // insert must accept a class Transaction with his elements
+    void insert(Transaction<Data>* transaction){
+        // *insert every element in a tree for later searches*
+        // <>inserting...</>
+        this->current->add_transaction(transaction);
+        if (this->current->get_size() == this->max_transactions){
+            // if current block is full, create a new one based on the previous hash
+            auto prev_hash = this->current->get_hash();
+            this->current->next = new Block<Data>(this->max_transactions);
+            this->current = this->current->next;
+            this->current->set_prev_hash(prev_hash);
+            this->current->set_precision(this->precision);
+            this->size++;
         }
     }
+
+    // cascade function starts when a transaction tried to be added into the blockchain
+    void modify(int indx, Transaction<Data>* transaction){
+        if (indx > this->size) return;
+        
+        auto temp = this->genesis->next;
+        int n = 1;
+        while (n != indx){
+            temp = temp->next;
+            n++;
+        }
+        temp->set_max_transactions(this->max_transactions + 1);
+        temp->add_transaction(transaction);
+        auto new_prev_hash = temp->mine();
+        n=0;
+        // modificamos hasta el ultimo bloque
+        // verificar si es necesario "&& temp->next->get_size() == this->max_transactions"
+        while(temp->next != nullptr){
+            temp = temp->next;
+            temp->set_prev_hash(new_prev_hash);
+            temp->mine();
+            new_prev_hash = temp->get_hash();
+            n++;
+        }
+        std::cout << "Cantidad de Bloques Modificados: " << n <<std::endl;
+    }
+    // metodo para agregar elementos al bloque hasta llenarlo y luego unirlo a la cadena
+    // no usado por actualizacion del sistema.
+    void add_block(Block<Data>* block){
+        block->set_precision(this->precision);
+        block->set_prev_hash(this->current->get_hash());
+        block->mine();
+        // unimos los bloques
+        this->current->next = block;
+        // actualizamos el ultimo bloque
+        this->current = block;
+        this->size++;
+    }
+
+    // Iteramos por los bloques e imprimimos sus datos
+    void print_chain(){
+        int n =1;
+        Block<Data> * temp = this->genesis->next;
+        while(temp != nullptr){
+            std::cout << "Block # " << n << ": "<< std::endl;
+            std::cout << "Hash: " << temp->get_hash() << std::endl;
+            std::cout << "Prev Hash: " << temp->get_prev_hash() << std::endl;
+//            std::cout << "Merkle Root: " << current->get_merkleroot() << std::endl;
+            std::cout << "Transactions: " << temp->get_transactions() << std::endl;
+            std::cout << "Nonce: " << temp->get_nonce() << std::endl;
+            std::cout << "Size: " << temp->get_size() << std::endl;
+            std::cout << "----------------" << std::endl;
+            temp = temp->next;
+            n++;
+        }
+    }
+    /*
     bool find_transaction(std::string _transaction){
         Block * current = this->genesis->next;
         while(current != nullptr){
@@ -48,17 +107,17 @@ public:
             current = current->next;
         }
         return false;
-    }
+    }*/
 
-    int get_size(){
+    int size_block() const{
         return this->size;
     }
 
     ~Blockchain(){
-        Block * current = this->genesis;
-        while(current != nullptr){
-            Block * temp = current;
-            current = current->next;
+        Block<Data> * current_ = this->genesis;
+        while(current_ != nullptr){
+            Block<Data> * temp = current_;
+            current_ = current_->next;
             delete temp;
         }
     }
--- sha256.h
+++ sha256.h
@@ -1,5 +1,3 @@
-#ifndef SHA256_H
-#define SHA256_H
 #include <string>
  
 class SHA256
@@ -50,4 +48,3 @@ std::string sha256(std::string input);
            | ((uint32) *((str) + 1) << 16)    \
            | ((uint32) *((str) + 0) << 24);   \
 }
-#endif
